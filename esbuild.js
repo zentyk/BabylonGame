5b46d9bc825878d1c732cb0861f06a2ff4fcc59d
Arturo Sanchez <asanchez@orthocana.com>
2022-07-21 17:19:53 -0500

    # MAIN/SETUP * Project setup.
    Signed-off-by: Arturo Sanchez <zentyk@protonmail.com>

---
+++ esbuild.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const handlebars = require('handlebars');
+const sass = require('sass');
+const esbuild = require('esbuild');
+const {watch} = require('chokidar');
+
+let compiled = handlebars.compile(fs.readFileSync('./src/index.hbs', 'utf8'));
+fs.writeFileSync('./dist/index.html', compiled({}));
+
+sass.compileAsync("./src/app.scss",{
+    sourceMap: true,
+    outputStyle: 'compressed'
+})
+    .then(result => {
+    fs.writeFileSync('./dist/app.css', result.css);
+})
+    .catch(err => {
+    console.log(err);
+    process.exit(1);
+});
+
+watch('./src/**/*.ts', {}).on('change', (path) => {
+    console.log(`building... ${path}`);
+    esbuild.build({
+        entryPoints: ['./src/app.ts'],
+        bundle: true,
+        sourcemap : true,
+        target : 'es2015',
+        minify : true,
+        outfile: './dist/app.js',
+        tsconfig: './tsconfig.json'
+    }).catch(() => process.exit(1));
+});
